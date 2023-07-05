60a120825269b4bf4a4ff3d4bfe46f66e0810585
danaster02 <106901641+danaster02@users.noreply.github.com>
2023-07-06 04:02:03 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+$(function() {
+ let header = $('.header');
+  
+ $(window).scroll(function() {
+   if($(this).scrollTop() > 1) {
+    header.addClass('header_fixed');
+   } else {
+    header.removeClass('header_fixed');
+   }
+ });
+});
+$(function() {
+    let header = $('.header');
+    let hederHeight = header.height() + 50; // вычисляем высоту шапки
+     
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1) {
+       header.addClass('header_fixed');
+       $('body').css({
+          'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
+       });
+      } else {
+       header.removeClass('header_fixed');
+       $('body').css({
+        'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+       })
+      }
+    });
+   });
